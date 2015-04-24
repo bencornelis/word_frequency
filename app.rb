bf13bb1ff8854a88abe3a858188225d8b26923b3
Ben Cornelis <bencornelis02@gmail.com>
2015-04-24 13:52:20 -0700

    Add /results route, app will return page to user containing word frequency.

--- app.rb
+++ app.rb
@@ -5,3 +5,10 @@ also_reload('lib/**/*.rb')
 get('/') do
   erb(:index)
 end
+
+get('/results') do
+  @word = params.fetch('word')
+  @sentence = params.fetch('sentence')
+  @frequency = @sentence.word_frequency(@word)
+  erb(:results)
+end
