599331da92a0686d4004261b0f31a5c971b0cf1d
Ben Cornelis <bencornelis02@gmail.com>
2015-04-24 13:26:47 -0700

    Method works on strings with up to two words.

--- lib/word_frequency.rb
+++ lib/word_frequency.rb
@@ -1,9 +1,15 @@
 class String
   define_method(:word_frequency) do |search_word|
-    if search_word.==(self)
-      1
-    else
-      0
+    words = self.split()
+    first_word = words[0]
+    second_word = words[1]
+    word_freq = 0
+    if first_word.==(search_word)
+      word_freq += 1
     end
+    if second_word.==(search_word)
+      word_freq += 1
+    end
+    word_freq
   end
 end
--- spec/word_frequency_spec.rb
+++ spec/word_frequency_spec.rb
@@ -9,4 +9,8 @@ describe('String#word_frequency') do
   it('returns the frequency when the argument is a word different from the string') do
     expect('bird'.word_frequency('sparrow')).to(eq(0))
   end
+
+  it('returns the frequency when the sentence is two words') do
+    expect('The bird'.word_frequency('bird')).to(eq(1))
+  end
 end
